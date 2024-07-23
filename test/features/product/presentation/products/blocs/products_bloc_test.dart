fc93ad2ac250ebd587585849c2e68b399c9b546d
bjrtdev <bj@risingtide.ph>
2024-07-23 23:02:54 +0800

    update bloc test cases

--- test/features/product/presentation/products/blocs/products_bloc_test.dart
+++ test/features/product/presentation/products/blocs/products_bloc_test.dart
@@ -3,38 +3,55 @@ import 'package:bloc_test/bloc_test.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:sprout_inventory/core/error/error_handler.dart';
+import 'package:sprout_inventory/core/error/failure.dart';
 import 'package:sprout_inventory/features/product/presentation/products/blocs/products_bloc.dart';
+import 'package:sprout_inventory/features/product/presentation/products/models/products_state_model.dart';
 import '../../../../../fixtures/stub_objects/products.dart';
 import '../../../domain/usecases/get_products_usecase_mock.mocks.dart';
 
 void main() {
   late MockGetProductsUseCase getProductsUseCase;
-  late ProductsBloc productsBloc;
+  late ProductsBloc bloc;
 
   setUp(() {
     getProductsUseCase = MockGetProductsUseCase();
-    productsBloc = ProductsBloc(getProductsUseCase);
+    bloc = ProductsBloc(getProductsUseCase);
   });
 
   test("initial state should be ProductsLoading", () {
     // assert
-    expect(productsBloc.state, equals(const ProductsLoading()));
+    expect(bloc.state, equals(const ProductsLoading()));
   });
 
   group("GetProducts", () {
-
-    void setUpMockGetProducts() => when(getProductsUseCase(any))
+    void setUpMockGetProductsSuccess() => when(getProductsUseCase(any))
             .thenAnswer((_) async => const Left(stubProducts));
 
+    // this test should fail i don't know why its passing
     blocTest<ProductsBloc, ProductsState>(
-      "should get data from GetProductsUseCase",
-      setUp: () => setUpMockGetProducts(),
-      build: () => productsBloc,
+      "should update the state to loaded state",
+      setUp: () => setUpMockGetProductsSuccess(),
+      build: () => bloc,
       act: (bloc) => bloc.add(const GetProducts()),
       verify: (_) => getProductsUseCase(stubProductsParam),
+      expect: () => []
+    );
+
+    void setUpMockGetProductsFailed() => when(getProductsUseCase(any))
+        .thenAnswer((_) async => const Right(ServerFailure()));
+
+    // this test should fail i don't know why its passing
+    blocTest<ProductsBloc, ProductsState>(
+      "should update the state to failed state",
+      setUp: () => setUpMockGetProductsFailed(),
+      build: () => bloc,
+      act: (bloc) => bloc.add(const GetProducts()),
+      verify: (_) => getProductsUseCase(stubProductsParam),
+      expect: () => [ ]
     );
   });
 
-  tearDown(() => productsBloc.close());
+  tearDown(() => bloc.close());
 
 }
