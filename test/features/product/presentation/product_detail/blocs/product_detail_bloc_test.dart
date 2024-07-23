8ffb361817002ee19eb981e298528ee6303b2b6c
Mikoto Suou <32681403+MikotoSuou@users.noreply.github.com>
2024-07-23 23:23:14 +0800

    Added bloc test cases (#5)
    * Update product_detail_bloc_test.dart
    
    - this update will fail the test
    - this is to test the CI workflow
    
    * Update product_detail_bloc_test.dart
    
    * update bloc test cases
    
    * Update products_bloc_test.dart
    
    * Update products_bloc_test.dart
    
    ---------
    
    Co-authored-by: bjrtdev <bj@risingtide.ph>

--- test/features/product/presentation/product_detail/blocs/product_detail_bloc_test.dart
+++ test/features/product/presentation/product_detail/blocs/product_detail_bloc_test.dart
@@ -3,6 +3,8 @@ import 'package:bloc_test/bloc_test.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:sprout_inventory/core/error/error_handler.dart';
+import 'package:sprout_inventory/core/error/failure.dart';
 import 'package:sprout_inventory/features/product/presentation/product_detail/blocs/product_detail_bloc.dart';
 import '../../../../../fixtures/stub_objects/product_detail.dart';
 import '../../../domain/usecases/get_product_detail_usecase_mock.mocks.dart';
@@ -22,18 +24,63 @@ void main() {
   });
 
   group("GetProductDetail", () {
-
-    void setUpMockGetProductDetail() => when(getProductDetailUseCase(any))
+    void setUpMockGetProductDetailSuccess() => when(getProductDetailUseCase(any))
             .thenAnswer((_) async => const Left(stubProductDetail));
     
     blocTest<ProductDetailBloc, ProductDetailState>(
-      "should get data from GetProductDetailUseCase",
-      setUp: () => setUpMockGetProductDetail(),
+      "should update the state to loaded state",
+      setUp: () => setUpMockGetProductDetailSuccess(),
       build: () => productDetailBloc,
       act: (bloc) => bloc.add(const GetProductDetail(productId: stubProductDetailParam)),
       verify: (_) => getProductDetailUseCase(stubProductDetailParam),
+      expect: () => [
+        const ProductDetailLoading(),
+        const ProductDetailLoaded(productDetail: stubProductDetail)
+      ]
+    );
+
+    void setUpMockGetProductDetailFailed() => when(getProductDetailUseCase(any))
+        .thenAnswer((_) async => const Right(ServerFailure()));
+
+    blocTest<ProductDetailBloc, ProductDetailState>(
+        "should update the state to failed state",
+        setUp: () => setUpMockGetProductDetailFailed(),
+        build: () => productDetailBloc,
+        act: (bloc) => bloc.add(const GetProductDetail(productId: stubProductDetailParam)),
+        verify: (_) => getProductDetailUseCase(stubProductDetailParam),
+        expect: () => [
+          const ProductDetailLoading(),
+          const ProductDetailLoadFailed(error: ResponseMessage.serverError)
+        ]
     );
   });
+
+
+  // group("GetProductDetail", () {
+  //   void setUpMockGetProductDetailSuccess() => when(getProductDetailUseCase(any))
+  //       .thenAnswer((_) async => const Left(stubProductDetail));
+  //
+  //   blocTest<ProductDetailCubit, ProductDetailState>(
+  //       "should update the state to loaded state",
+  //       setUp: () => setUpMockGetProductDetailSuccess(),
+  //       build: () => cubit,
+  //       act: (cubit) => cubit.getProductDetail(productId: stubProductDetailParam),
+  //       verify: (_) => getProductDetailUseCase(stubProductDetailParam),
+  //       expect: () => [ const ProductDetailState.loaded(stubProductDetail) ]
+  //   );
+  //
+  //   void setUpMockGetProductDetailFailed() => when(getProductDetailUseCase(any))
+  //       .thenAnswer((_) async => const Right(ServerFailure()));
+  //
+  //   blocTest<ProductDetailCubit, ProductDetailState>(
+  //       "should update the state to failed state",
+  //       setUp: () => setUpMockGetProductDetailFailed(),
+  //       build: () => cubit,
+  //       act: (cubit) => cubit.getProductDetail(productId: stubProductDetailParam),
+  //       verify: (_) => getProductDetailUseCase(stubProductDetailParam),
+  //       expect: () => [ const ProductDetailState.failed(ResponseMessage.serverError) ]
+  //   );
+  // });
 
   tearDown(() => productDetailBloc.close());
 
